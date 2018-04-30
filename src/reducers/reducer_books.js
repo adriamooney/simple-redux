719365e08dcf3542f23a6405d95dfc96a0df86fc
adria mooney <adriamooney@gmail.com>
2018-04-30 13:53:41 -0700

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'javascript: the good parts', pages: 40},
+    {title: 'harry potter', pages:100},
+    {title: 'dark tower', pages: 124},
+    {title: 'some other book', pages:323}
+  ]
+}
